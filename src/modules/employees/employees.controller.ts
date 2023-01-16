ac3854faf652fac9c407f904f0e3302e06cb1a32
Kochiyama <kochiyama.pro@gmail.com>
2023-01-16 09:42:29 -0400

    add auth guard

--- src/modules/employees/employees.controller.ts
+++ src/modules/employees/employees.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
